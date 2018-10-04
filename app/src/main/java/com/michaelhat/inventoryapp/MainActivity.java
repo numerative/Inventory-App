f3584bab30476b3471a708604ffe52398ccba38d
numerative <michael.hathi@gmail.com>
2018-10-04 16:18:07 +0530

    Migrate to androidx packages and add MDC package

--- app/src/main/java/com/michaelhat/inventoryapp/MainActivity.java
+++ app/src/main/java/com/michaelhat/inventoryapp/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.michaelhat.inventoryapp.InventoryContract.StockEntry;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
