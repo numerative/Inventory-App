255a0f545833a06f72aed52bbb7876af772ec6e9
numerative <michael.hathi@gmail.com>
2018-10-24 16:46:07 +0530

    Products can now be edited

--- app/src/main/java/com/michaelhat/inventoryapp/DetailScreenActivity.java
+++ app/src/main/java/com/michaelhat/inventoryapp/DetailScreenActivity.java
@@ -1,16 +1,20 @@
 package com.michaelhat.inventoryapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.michaelhat.inventoryapp.InventoryContract.ProductEntry;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -84,5 +88,60 @@ public class DetailScreenActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail_screen_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        if (currentUri == null) {
+            MenuItem deleteMenuItem = menu.findItem(R.id.action_single_delete);
+            MenuItem editMenuItem = menu.findItem(R.id.edit_record);
+            deleteMenuItem.setVisible(false);
+            editMenuItem.setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                saveProduct();
+                finish();
+                break;
+            case R.id.edit_record:
+                break;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(DetailScreenActivity.this);
+                break;
+        }
+        return true;
+    }
+
+    private void saveProduct() {
+        //Prepare the contentValues
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, productNameEditText.getText().toString().trim());
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, priceEditText.getText().toString().trim());
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, quantityEditText.getText().toString().trim());
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE, supplierPhoneEditText.getText().toString().trim());
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, supplierNameEditText.getText().toString().trim());
+
+        if (currentUri != null) {
+            int rowsUpdated = getContentResolver().update(currentUri, values, null, null);
+            displayEditToast(rowsUpdated);
+        } else {
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void displayEditToast(int rowsUpdated) {
+        switch (rowsUpdated) {
+            case 0:
+                Toast.makeText(this, R.string.toast_edit_error, Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(this, R.string.toast_edit_saved, Toast.LENGTH_SHORT).show();
+        }
     }
 }
