4663f78703b909b36c1a47d81b38c0dc53d88657
numerative <michael.hathi@gmail.com>
2018-10-25 12:11:47 +0530

    Sale button working

--- app/src/main/java/com/michaelhat/inventoryapp/ProductCursorApdapter.java
+++ app/src/main/java/com/michaelhat/inventoryapp/ProductCursorApdapter.java
@@ -1,9 +1,10 @@
 package com.michaelhat.inventoryapp;
 
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,26 +41,34 @@ public class ProductCursorApdapter extends CursorAdapter {
         String productQuantity = String.valueOf(
                 cursor.getInt(cursor.getColumnIndexOrThrow(ProductEntry.COLUMN_PRODUCT_QUANTITY)));
 
-        //Code not working
-        //Reduce Quantity by 1
-        int afterSaleQuantity = Integer.valueOf(productQuantity) - 1;
-        Log.v("after sale", String.valueOf(afterSaleQuantity));
-        final ContentValues quantityAfterSaleForDb = new ContentValues();
-        quantityAfterSaleForDb.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, afterSaleQuantity);
-
-/*        saleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri productUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, cursor.getLong(cursor.getColumnIndexOrThrow(ProductEntry._ID)));
-                Log.v("Uri", String.valueOf(productUri));
-                int rowsUpdated = context.getContentResolver().update(productUri, quantityAfterSaleForDb, null, null);
-                Log.v("Rows updated", String.valueOf(rowsUpdated));
-            }
-        });*/
+        //Sale Button
+        saleButton.setOnClickListener(new saleButtonClickListener(context, cursor, productQuantity));
 
         //Set text
         productNameView.setText(productName);
         productPriceView.setText(productPrice);
         productQuantityView.setText(productQuantity);
+    }
+
+    //Custom OnClickListener
+    public class saleButtonClickListener implements View.OnClickListener {
+        Context context;
+        Cursor cursor;
+        String productQuantity;
+
+        public saleButtonClickListener(Context context, Cursor cursor, String productQuantity) {
+            this.context = context;
+            this.cursor = cursor;
+            this.productQuantity = productQuantity;
+        }
+
+        @Override
+        public void onClick(View view) {
+            int afterSaleQuantity = Integer.valueOf(productQuantity) - 1;
+            ContentValues quantityAfterSaleForDb = new ContentValues();
+            quantityAfterSaleForDb.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, afterSaleQuantity);
+            Uri productUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, cursor.getLong(cursor.getColumnIndexOrThrow(ProductEntry._ID)));
+            context.getContentResolver().update(productUri, quantityAfterSaleForDb, null, null);
+        }
     }
 }
